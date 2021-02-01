1a1a2cd916c63611731a5b6a054aac19ab97d345
vladsergy <drylb924@gmail.com>
2021-02-01 22:54:47 +0200

    exchange files content using Promises

---
+++ src/exchange.js
@@ -0,0 +1,27 @@
+// @ts-check
+/* eslint-disable import/prefer-default-export */
+import { promises as fs } from 'fs';
+
+// BEGIN (write your solution here)
+const exchange = async (filePath1, filePath2) => {
+  const reading1 = fs.readFile(filePath1);
+  const reading2 = fs.readFile(filePath2);
+  const [data1, data2] = await Promise.all([reading1, reading2]);
+
+  const writing1 = fs.writeFile(filePath1, data2);
+  const writing2 = fs.writeFile(filePath2, data1);
+  await Promise.all([writing1, writing2]);
+};
+
+export default exchange;
+
+// END
+
+/* file.js
+Реализуйте и экспортируйте асинхронную функцию exchange(),
+которая обменивает содержимое двух файлов.
+
+import { exchange } from './file.js';
+
+exchange('/myfile1', '/myfile2');
+ */
